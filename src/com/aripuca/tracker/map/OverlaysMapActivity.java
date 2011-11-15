98b5dd5f5c1e9c7d9b87965dd9d100a0da596714
vitalyn <vitalyn@VITALY-PC.nmlsinc.com>
2011-11-15 08:48:41 -0700

    test lab

--- src/com/aripuca/tracker/map/OverlaysMapActivity.java
+++ src/com/aripuca/tracker/map/OverlaysMapActivity.java
@@ -3,6 +3,8 @@ package com.aripuca.tracker.map;
 import java.util.ArrayList;
 import java.util.List;
 
+import android.app.AlertDialog;
+import android.content.Context;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -55,42 +57,49 @@ public class OverlaysMapActivity extends MapActivity {
 	private int minLng = (int) (180 * 1E6);
 	private int maxLng = (int) (-180 * 1E6);
 
-	private ArrayList<OverlayItem> overlayItems = new ArrayList<OverlayItem>();
-
 	/**
 	 * Map overlay class
 	 */
-	class MyItemizedOverlay extends com.google.android.maps.ItemizedOverlay {
+	class MyItemizedOverlay extends com.google.android.maps.ItemizedOverlay<OverlayItem> {
 
-		public MyItemizedOverlay(Drawable defaultMarker) {
-			super(defaultMarker);
-			populate();
+		private ArrayList<OverlayItem> overlayItems = new ArrayList<OverlayItem>();
+
+		private Context mContext;
+
+		public MyItemizedOverlay(Drawable defaultMarker, Context context) {
+			super(boundCenterBottom(defaultMarker));
+			mContext = context;
 		}
 
-		/**
-		 * Overridden draw method
-		 */
-		@Override
-		public boolean draw(Canvas canvas, MapView mapView, boolean shadow, long when) {
+		public MyItemizedOverlay(Drawable defaultMarker) {
+			super(boundCenterBottom(defaultMarker));
+		}
 
-			super.draw(canvas, mapView, shadow);
+		public void addOverlay(OverlayItem overlay) {
+			overlayItems.add(overlay);
 			populate();
-			return true;
-
 		}
 
 		@Override
 		protected OverlayItem createItem(int i) {
-			// TODO Auto-generated method stub
-			return null;
+			return overlayItems.get(i);
 		}
 
 		@Override
 		public int size() {
-			// TODO Auto-generated method stub
-			return 0;
+			return overlayItems.size();
 		}
 
+		@Override
+		protected boolean onTap(int index) {
+			OverlayItem item = overlayItems.get(index);
+			AlertDialog.Builder dialog = new AlertDialog.Builder(mContext);
+			dialog.setTitle(item.getTitle());
+			dialog.setMessage(item.getSnippet());
+			dialog.show();
+			return true;
+		}
+		
 	}
 
 	/**
@@ -113,19 +122,25 @@ public class OverlaysMapActivity extends MapActivity {
 		mapView.setBuiltInZoomControls(true);
 
 		// ---Add a location marker---
-//		MyItemizedOverlay mapOverlay = new MyItemizedOverlay();
-
-		Double lat = 37.422006 * 1E6;
-		Double lng = -122.084095 * 1E6;
-		GeoPoint point = new GeoPoint(lat.intValue(), lng.intValue());
-		OverlayItem oi = new OverlayItem(point, "Marker", "Marker Text");
-
-		List<Overlay> listOfOverlays = mapView.getOverlays();
-		listOfOverlays.clear();
-//		listOfOverlays.add();
-
+		List<Overlay> mapOverlays = mapView.getOverlays();
+		Drawable drawable = this.getResources().getDrawable(R.drawable.map_pin);
+		drawable.setBounds(0, 0, drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight());
+		
+		MyItemizedOverlay itemizedoverlay = new MyItemizedOverlay(drawable, this);
+		
+		GeoPoint point = new GeoPoint(19240000,-99120000);
+		OverlayItem overlayitem = new OverlayItem(point, "Hola, Mundo!", "I'm in Mexico City!");
+		
+		GeoPoint point2 = new GeoPoint(35410000, 139460000);
+		OverlayItem overlayitem2 = new OverlayItem(point2, "Sekai, konichiwa!", "I'm in Japan!");		
+		
+		itemizedoverlay.addOverlay(overlayitem);
+		itemizedoverlay.addOverlay(overlayitem2);
+		
+		mapOverlays.add(itemizedoverlay);	
+		
 		mapView.invalidate();
-
+		
 	}
 
 	/**
