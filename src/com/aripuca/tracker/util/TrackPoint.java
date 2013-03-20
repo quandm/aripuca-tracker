446f0d0eb588639dcd5e84432e571f380e9e6df5
vitalnik <vitalnik@localhost>
2013-03-20 17:32:40 -0600

    map view updates

--- src/com/aripuca/tracker/util/TrackPoint.java
+++ src/com/aripuca/tracker/util/TrackPoint.java
@@ -7,6 +7,10 @@ public class TrackPoint extends Object {
 	private GeoPoint geoPoint;
 
 	private int segmentIndex;
+	
+	private float speed; 
+	private float distance; 
+	private float elevation; 
 
 	public TrackPoint(GeoPoint gp, int si) {
 
@@ -14,13 +18,37 @@ public class TrackPoint extends Object {
 		this.segmentIndex = si;
 
 	}
-
+	
 	public GeoPoint getGeoPoint() {
 		return this.geoPoint;
 	}
 
 	public int getSegmentIndex() {
 		return this.segmentIndex;
+	}
+	
+	public void setSpeed(float s) {
+		this.speed = s;
+	}
+	
+	public float getSpeed() {
+		return this.speed;
+	}
+
+	public void setDistance(float d) {
+		this.distance = d;
+	}
+	
+	public float getDistance() {
+		return this.distance;
+	}
+
+	public void setElevation(float e) {
+		this.elevation = e;
+	}
+	
+	public float getElevation() {
+		return this.elevation;
 	}
 
 }
